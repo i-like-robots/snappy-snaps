0fd231e346d10596caf81e47f91f116db9feb891
Matt Hinchliffe <matt@maketea.co.uk>
2023-10-12 21:24:44 +0100

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  env: {
+    es2022: true,
+    node: true,
+    browser: false,
+  },
+  parserOptions: {
+    sourceType: 'module',
+  },
+  extends: ['eslint:recommended'],
+}
